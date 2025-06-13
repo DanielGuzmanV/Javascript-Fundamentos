539dd6db915960245cf987020d89f85a5851d2d2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-13 10:50:43 -0400

    inicio de los archivos de tailwind

---
+++ Ejemplos/DOM/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./ejemplo-*/**/*.html", "./ejemplo-*/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
